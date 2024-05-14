4cc492541f0e0af5640cec4b6aa8549ad5bba020
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-05-14 23:41:15 +0600

    scatter()

--- 01_matplotlib_tutorial.py
+++ 01_matplotlib_tutorial.py
@@ -27,7 +27,7 @@ plt.show()
 For every x, y pair of arguments, there is an optional third argument which is the format string that indicates the color and line type of the plot. The letters and symbols of the format string are from MATLAB, and you concatenate a color string with a line style string. The default format string is 'b-', which is a solid blue line. For example, to plot the above with red circles, you would issue
 """
 
-plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+|plt.plot([1,2,3,4], [1,4,9,16], 'ro')
 plt.axis((0, 6,0,20))
 plt.show()
 
@@ -48,9 +48,28 @@ plt.show()
 import numpy as np
 
 # evenly sampled time at 200ms intervals
-t = np.arange(0., 4., 0.2)
+t = np.arange(0., 5., 0.2)
 
 # red dashes, blue squares and green triangles
-plt.plot(t, t, 'r--', t, t**2, 'bs', t, t,**3, 'g')
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**2.5, 'y+', t, t**3, 'g^')
+plt.show()
+
+"""#### Plotting with keyword strings"""
+
+data = {
+          'a': np.arange(50),
+          'c': np.random.randint(0, 50, 50),
+          'd': np.random.randn(50)
+        }
+
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d'] * 100)
+
+data
+print(f"length of data= {len(data)}")
+
+plt.scatter('a', 'b', c='c', s='d', data=data)
+plt.xlabel('entry a')
+plt.ylabel('entry b')
 plt.show()
 
